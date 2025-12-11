813849162f460e4de92cac0ca2d8b9d0eb4d9003
1542761533 <1542761533@qq.com>
2025-12-11 12:47:43 +0800

    update

--- neither/custom.markers.js
+++ neither/custom.markers.js
@@ -1,53 +1,27 @@
-/*
-
-This is a JavaScript file you can edit to add custom markers to the map.
-uNmINeD does not overwrite this file during map generation.
-
-Steps:
-
-    1. Edit this file using Notepad or a code editor (do not use document editors like Microsoft Word)
-    2. Change the line "isEnabled: false," to "isEnabled: true," to display the markers
-    3. Change or remove the example markers
-    4. Add your own markers
-
-Marker format:
-
-    {
-        x: X coordinate of the marker (in Minecraft block units),
-        z: Z coordinate of the marker (in Minecraft block units),
-        image: marker image URL to display (in quotes),
-        imageScale: scale of the image (e.g. 1 = display full size, 0.5 = display half size),
-        imageAnchor: [0.5, 1] means the tip of the pin is at the center-bottom of the image (see OpenLayers documentation for more info),
-        text: marker text do display (in quotes),
-        textColor: text color in HTML/CSS format (in quotes),
-        offsetX: horizontal pixel offset of the text,
-        offsetY: vertical pixel offset of the text,
-        font: text font in HTML/CSS format (in quotes),
-    },
-
-Things to keep in mind:
-
-* There are opening and closing brackets for each marker "{" and "}"
-* Property names are case sensitive (i.e. "textColor" is okay, "TextColor" is not)
-* There is a comma (",") at the end of each line except the opening brackets ("{")
-
-You can use https://mapmarker.io/editor to generate custom pin images.
-Use the imageScale property if the pin image is too large.
-
-*/
-
 UnminedCustomMarkers = {
 
     isEnabled: true,
 
     markers: [
-{
+        {
             x: 438,
             z: 313,
             image: "custom.pin.png",
             imageAnchor: [0.5, 1],
             imageScale: 0.5,
             text: "猪灵交易",
+            textColor: "purple",
+            offsetX: 0,
+            offsetY: 20,
+            font: "bold 16px Arial",
+        },
+        {
+            x: -134,
+            z: -19,
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+            text: "烈焰人农场",
             textColor: "purple",
             offsetX: 0,
             offsetY: 20,
--- overworld/custom.markers.js
+++ overworld/custom.markers.js
@@ -1,41 +1,3 @@
-/*
-
-This is a JavaScript file you can edit to add custom markers to the map.
-uNmINeD does not overwrite this file during map generation.
-
-Steps:
-
-    1. Edit this file using Notepad or a code editor (do not use document editors like Microsoft Word)
-    2. Change the line "isEnabled: false," to "isEnabled: true," to display the markers
-    3. Change or remove the example markers
-    4. Add your own markers
-
-Marker format:
-
-    {
-        x: X coordinate of the marker (in Minecraft block units),
-        z: Z coordinate of the marker (in Minecraft block units),
-        image: marker image URL to display (in quotes),
-        imageScale: scale of the image (e.g. 1 = display full size, 0.5 = display half size),
-        imageAnchor: [0.5, 1] means the tip of the pin is at the center-bottom of the image (see OpenLayers documentation for more info),
-        text: marker text do display (in quotes),
-        textColor: text color in HTML/CSS format (in quotes),
-        offsetX: horizontal pixel offset of the text,
-        offsetY: vertical pixel offset of the text,
-        font: text font in HTML/CSS format (in quotes),
-    },
-
-Things to keep in mind:
-
-* There are opening and closing brackets for each marker "{" and "}"
-* Property names are case sensitive (i.e. "textColor" is okay, "TextColor" is not)
-* There is a comma (",") at the end of each line except the opening brackets ("{")
-
-You can use https://mapmarker.io/editor to generate custom pin images.
-Use the imageScale property if the pin image is too large.
-
-*/
-
 UnminedCustomMarkers = {
 
     isEnabled: true,
@@ -49,7 +11,43 @@ UnminedCustomMarkers = {
             image: "custom.pin.png",
             imageAnchor: [0.5, 1],
             imageScale: 0.5,
-            text: "刷铁机",
+            text: "四联刷铁机",
+            textColor: "purple",
+            offsetX: 0,
+            offsetY: 20,
+            font: "bold 16px Arial",
+        },
+        {
+            x: 147,
+            z: -593,
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+            text: "骨粉机",
+            textColor: "purple",
+            offsetX: 0,
+            offsetY: 20,
+            font: "bold 16px Arial",
+        },
+        {
+            x: 6170,
+            z: 3363,
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+            text: "女巫塔",
+            textColor: "purple",
+            offsetX: 0,
+            offsetY: 20,
+            font: "bold 16px Arial",
+        },
+        {
+            x: 226,
+            z: -5299,
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+            text: "8*刷沙机",
             textColor: "purple",
             offsetX: 0,
             offsetY: 20,
@@ -67,11 +65,41 @@ UnminedCustomMarkers = {
             offsetY: 20,
             font: "bold 16px Arial",
         },
-        // add your markers here
-
-
-
-
-        // do not delete the following two closing brackets
+        {
+            x: 574,
+            z: -434,
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+            text: "竹子机",
+            textColor: "purple",
+            offsetX: 0,
+            offsetY: 20,
+            font: "bold 16px Arial",
+        },
+        {
+            x: -1050,
+            z: -453,
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+            text: "刷冰机",
+            textColor: "purple",
+            offsetX: 0,
+            offsetY: 20,
+            font: "bold 16px Arial",
+        },
+        {
+            x: 6010,
+            z: 3362,
+            image: "custom.pin.png",
+            imageAnchor: [0.5, 1],
+            imageScale: 0.5,
+            text: "史莱姆农场",
+            textColor: "purple",
+            offsetX: 0,
+            offsetY: 20,
+            font: "bold 16px Arial",
+        },
     ]
 }
